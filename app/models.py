0e804cab5f533f7dd009cd9b18af607f48f1f5b2
JamisenCold <zhihaoc845@gmail.com>
2024-12-26 15:23:57 +0800

    Add file

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Date, Integer, String, Boolean
+from .database import Base
+
+# Define Model
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
+    due_date= Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
